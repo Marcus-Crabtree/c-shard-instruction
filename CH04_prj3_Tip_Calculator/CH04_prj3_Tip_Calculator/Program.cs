346c158af7e72a75f11fa819c94ef55cf2bfb02e
Marcus Crabtree <kmarcuscrabtree@gmail.com>
2020-04-15 12:35:28 -0400

    ch05

---
+++ CH04_prj3_Tip_Calculator/CH04_prj3_Tip_Calculator/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace CH04_prj3_Tip_Calculator {
+    class Program {
+        static void Main(string[] args) {
+            double mealCost = 0.0;
+            string choice = "y";
+            double rate = 0.0;
+            double tipAmount = mealCost + rate;
+            double mealCostFinal =mealCost+tipAmount;
+            Console.WriteLine("Tip Calculator");
+            Console.WriteLine();
+            while (choice.Contains("y")) {
+                Console.WriteLine("please enter the cost of meal: ");
+                mealCost = Convert.ToDouble(Console.ReadLine());
+                for (rate = .15; rate <.30; rate +=.05) {
+                    tipAmount = mealCost * rate;
+                    mealCostFinal = mealCost + tipAmount;
+                }
+                Console.WriteLine (rate + tipAmount + mealCostFinal);
+                Console.Write("would you like to calculate another tip? (y/n): ");
+                choice = Console.ReadLine();
+               
+            }
+
+    Console.WriteLine("Goodbye!");
+        }
+    }
+}
---
+++ CH05_methods/CH05_methods/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace CH05_methods {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Write some methods");
+
+            int nbr = GetInt();
+
+            Console.WriteLine("Number entered = " + nbr);
+
+
+
+
+
+
+
+
+
+        }
+
+        private static int GetInt() {
+            //prompt the user for input 
+            Console.Write("Enter a whole number: ");
+            //accept input asa string
+            string nbrStr = Console.ReadLine();
+            // convert that sting to an int
+            int nbr = int.Parse(nbrStr);
+            return nbr;
+        }
+    }
+}
