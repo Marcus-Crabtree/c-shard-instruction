7872eebe434728a2a5054ff4250cdb4061b358c5
Marcus Crabtree <kmarcuscrabtree@gmail.com>
2020-04-15 11:12:35 -0400

    update ch04

--- CH04_Control_Structure/CH04_Control_Structure/Program.cs
+++ CH04_Control_Structure/CH04_Control_Structure/Program.cs
@@ -3,7 +3,49 @@
 namespace CH04_Control_Structure {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to the control structures app");
+
+            // command menu example
+            string command = "";
+            Console.WriteLine("COMMAND MENU");
+            Console.WriteLine("list - list all");
+            Console.WriteLine("add -  add an item");
+            Console.WriteLine("exit - exit loop");
+            while (!command.Contains("exit")) {
+                Console.Write("Enter a command: ");
+                command = Console.ReadLine();
+                switch (command) {
+                    case "list":
+                        Console.WriteLine("List selected");
+                        break;
+                    case "add":
+                        Console.WriteLine("add selected");
+                        break;
+                    case "exit":
+                        Console.WriteLine("exit selected");
+                        break;
+                    default:
+                        Console.WriteLine("invalid option");
+                        break;
+                }
+
+
+            }
+
+            string[] names = { "Sarah", "Sam", "tyler", "jacob" };
+            // in java for(type t: collection)
+            foreach (string name in names) {
+                Console.WriteLine(name);
+                }
+            Console.WriteLine("for loop");
+            for (int i = 0; i < names.Length; i++) {
+                Console.WriteLine(names[i]);
+            }
+
+            Console.WriteLine(names); //doesnt work
+
+                Console.WriteLine("Goodbye");
+            }
         }
     }
-}
+
