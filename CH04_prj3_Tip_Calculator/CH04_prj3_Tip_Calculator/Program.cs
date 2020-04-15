1c784f5d1d562e9831970fbda9f5e2371962b1ed
Marcus Crabtree <kmarcuscrabtree@gmail.com>
2020-04-15 13:36:03 -0400

    tip calc && ch05meth

--- CH04_prj3_Tip_Calculator/CH04_prj3_Tip_Calculator/Program.cs
+++ CH04_prj3_Tip_Calculator/CH04_prj3_Tip_Calculator/Program.cs
@@ -6,24 +6,33 @@ namespace CH04_prj3_Tip_Calculator {
             double mealCost = 0.0;
             string choice = "y";
             double rate = 0.0;
-            double tipAmount = mealCost + rate;
-            double mealCostFinal =mealCost+tipAmount;
+            double tipAmount = 0.0; //mealCost + rate;
+            double mealCostFinal = 0.0; // mealCost + tipAmount;
             Console.WriteLine("Tip Calculator");
             Console.WriteLine();
             while (choice.Contains("y")) {
                 Console.WriteLine("please enter the cost of meal: ");
                 mealCost = Convert.ToDouble(Console.ReadLine());
-                for (rate = .15; rate <.30; rate +=.05) {
+                /*
+                for (rate = .15; rate < .30; rate += .05)
+                {
                     tipAmount = mealCost * rate;
                     mealCostFinal = mealCost + tipAmount;
                 }
-                Console.WriteLine (rate + tipAmount + mealCostFinal);
+                */
+                for (rate = 0.15; rate <= 0.25; rate += 0.05) //0.15, 0.20, 0.25
+                {
+                    tipAmount = rate * mealCost;
+                    mealCostFinal = tipAmount + mealCost;
+                    Console.WriteLine(rate);
+                    Console.WriteLine("Tip amount: " + tipAmount);
+                    Console.WriteLine("Total amount: " + mealCostFinal);
+                }
+                //Console.WriteLine (rate + tipAmount + mealCostFinal);
                 Console.Write("would you like to calculate another tip? (y/n): ");
                 choice = Console.ReadLine();
-               
             }
-
-    Console.WriteLine("Goodbye!");
+            Console.WriteLine("Goodbye!")
         }
     }
 }
--- CH05_methods/CH05_methods/Program.cs
+++ CH05_methods/CH05_methods/Program.cs
@@ -5,11 +5,12 @@ namespace CH05_methods {
         static void Main(string[] args) {
             Console.WriteLine("Write some methods");
 
-            int nbr = GetInt();
+            int nbr = GetInt("Enter a whole number: ");
 
             Console.WriteLine("Number entered = " + nbr);
 
-
+            Console.WriteLine("sum 3 numbers = " + sum(1, 2, 3));
+            Console.WriteLine("sum 2 numbers = " + sum(1, 2));
 
 
 
@@ -19,14 +20,21 @@ namespace CH05_methods {
 
         }
 
-        private static int GetInt() {
+        private static int GetInt(string prompt) {
             //prompt the user for input 
-            Console.Write("Enter a whole number: ");
+            Console.Write(prompt);
             //accept input asa string
             string nbrStr = Console.ReadLine();
             // convert that sting to an int
             int nbr = int.Parse(nbrStr);
             return nbr;
         }
+   private static int sum(int n1, int n2, int n3=0) {
+            int sum = n1+n2+n3;
+
+
+            return sum;
+        }
+    
     }
 }
