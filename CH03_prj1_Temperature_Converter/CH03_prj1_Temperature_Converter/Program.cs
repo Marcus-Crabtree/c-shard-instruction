bf397870b5f43284ebb6e24d2a36e149d3f8b39c
Marcus Crabtree <kmarcuscrabtree@gmail.com>
2020-04-15 11:33:30 -0400

    ch03 prj1

---
+++ CH03_prj1_Temperature_Converter/CH03_prj1_Temperature_Converter/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CH03_prj1_Temperature_Converter {
+    class Program {
+        static void Main(string[] args) {
+            string choice = "y";
+            double tempF = 0.0;
+            double tempC = 0.0;
+            Console.WriteLine("Welcome to the Temperature Converter.");
+            while (choice.Contains("y")) {
+                Console.Write("Enter degrees in Fahrenheit: ");
+                tempF = Convert.ToDouble(Console.ReadLine());
+                tempC = (tempF - 32) * 5 / 9;
+
+                Console.WriteLine("Degrees in Celsius: " + Math.Round(tempC, 2,MidpointRounding.ToEven));
+                Console.Write("Press 'y' to continue, 'n' to exit: ");
+                choice = Console.ReadLine();
+            }
+
+            Console.WriteLine("Good bye!");
+        }
+    }
+}
